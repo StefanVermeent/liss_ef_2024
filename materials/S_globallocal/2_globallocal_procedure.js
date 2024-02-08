aadeea1059fbaa6cd7888522ffddc8c92d42186e
Stefan <p.c.s.vermeent@gmail.com>
2024-02-08 13:35:18 +0100

    add prompt to 2nd batch of global local trials

--- materials/S_globallocal/2_globallocal_procedure.js
+++ materials/S_globallocal/2_globallocal_procedure.js
@@ -68,6 +68,7 @@ var globallocal_procedure02 = {
   incorrect_text: "",
   feedback_duration: 1000,
   show_stim_with_feedback: false,
+  prompt: "<br><br><div style='width: 600px;'><h1 style='float: left; font-size: 20px; margin:0;'>GROOT<br><br>A</h1><h1 style='float: right; margin:0; font-size: 20px;'>KLEIN<br><br>L</h1></div>",
   stimulus: function(){return jsPsych.timelineVariable('stimulus')},
   data: {
     rule: function(){return jsPsych.timelineVariable('data')['rule']},
